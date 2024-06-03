79aa39591166e1a38fe0533a0d4237765cc719ec
Kedarnath <kedarrothe05@gmail.com>
2024-06-03 15:19:48 +0200

    First Commit

---
+++ script.js
@@ -0,0 +1,13 @@
+function toggleMenu() {
+    const navLinks = document.querySelector('.nav-links');
+    navLinks.classList.toggle('show');
+}
+
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function(e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
